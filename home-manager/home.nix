677f49216e071dfb0a09f04171b010620490cc1f
Shawn Thiah <shawnthiah@gmail.com>
2026-03-02 22:06:46 +0800

    add jj git push shortcut

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -204,6 +204,10 @@ in
       setopt AUTO_PUSHD
       setopt PUSHD_IGNORE_DUPS
 
+      jjpub() {
+        jj bookmark set main -r @- && jj git push -b main
+      }
+
       if command -v gpgconf >/dev/null 2>&1 && [ -d "$HOME/.gnupg" ]; then
         unset SSH_AGENT_PID
         if [ "''${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
